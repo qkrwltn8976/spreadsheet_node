9f4e8898656346b3cfa5cdb1b4e8301f7e099608
qkrwltn8976 <qkrwltn8976@naver.com>
2019-08-19 17:46:59 +0900

    created json file

--- index.js
+++ index.js
@@ -4,17 +4,40 @@ const googleSpreadsheet = require('google-spreadsheet');
 const creds = require('./spreadsheetwillson-212291344f35.json');
 const doc = new googleSpreadsheet('1amaqoQpOL5EWiYiPf6jJ4EQ-y7MZz-SKeUidGwWfNZc');
 
+const http = require('http');
+const url = require('url');
+const gsjson = require('google-spreadsheet-to-json');
+const fs = require('fs')
+const path = require('path')
+const stringify = require('stringify');
+
 doc.useServiceAccountAuth(creds, function(err) {
-    doc.getInfo(function(err, info) {
-        //console.log(info);
-        console.log("구글 시트의 제목  : " + info.title);
+    var json = [];
 
-		console.log("구글 시트의 URL  : " + info.id);
+    doc.getRows(
+        1
+        ,{
+            'offset': 1
+        }
+        , function(err, rows) {
+            for(var i=0; rows.length > i; i++){
+                var obj = new Object();
+                obj.phone = rows[i].phone;
+                obj.nickname = rows[i].nickname;
+                obj.age = rows[i].age;
+                obj.askcategory = rows[i].askcategory;
+                obj.askcontent = rows[i].askcontent;
+                obj.willsongender = rows[i].willsongender;
+                obj.willsoncareer = rows[i].willsoncareer;
+                obj.willsonstyle = rows[i].willsonstyle;
 
-        console.log("마지막으로 업데이트된 날짜 및 시간  : " + info.updated);
+                json.push(obj);
+            } 
+                
+            fs.writeFileSync(path.resolve(__dirname, './data.json'), JSON.stringify(json, null, '\t'));
+            console.log('json 파일 생성');
+        }
+    )
 
-		console.log("스프레드시트의 생성자 아이디  : " + info.author.name);
-
-		console.log("스프레드시트의 생성자 메일주소  : " + info.author.email);
-    })
 });
+
